f55cdd9d31b1e765162fd3ce3d4aaef542b17792
Joseph Yu <josephyu2712+github@gmail.com>
2020-05-21 19:03:14 +0100

    Initial commit

---
+++ package.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+name = 'bat'
+
+# Vendor packages: <vendor_version>+local.<our_version>
+__version__ = '0.15.1'
+version = __version__ + '+local.1.0.0'
+
+description = 'The Python IDE for Professional Developers'
+
+authors = ['sharkdp', 'Joseph Yu']
+
+variants = [['platform-linux', 'arch-x86_64']]
+
+tools = ['bat']
+# @late()
+# def tools():
+#     import os
+#     bin_path = os.path.join(str(this.root), 'bin')
+#     executables = []
+#     for item in os.listdir(bin_path):
+#         path = os.path.join(bin_path, item)
+#         if os.access(path, os.X_OK) and not os.path.isdir(path):
+#             executables.append(item)
+#     return executables
+
+
+build_command = r'''
+set -euf -o pipefail
+
+# Setup: curl "{CURL_FLAGS}" ...
+# Show progress bar if output to terminal, else silence
+declare -a CURL_FLAGS
+CURL_FLAGS=("-L")
+[ -t 1 ] && CURL_FLAGS+=("-#") || CURL_FLAGS+=("-sS")
+
+FORK="https://github.com/sharkdp/bat"
+URL="$FORK"/releases/download/"v{version}"
+URL+=/bat-v{version}-"$REZ_ARCH_VERSION"-unknown-linux-musl.tar.gz
+
+if [[ $REZ_BUILD_INSTALL -eq 1 ]]
+then
+    set -x
+    curl "{CURL_FLAGS}" "$URL" \
+    | tar --strip-components=1 -xz -C "$REZ_BUILD_INSTALL_PATH"
+fi
+
+'''.format(version=__version__, CURL_FLAGS='${{CURL_FLAGS[@]}}')
+
+
+def commands():
+    """Commands to set up environment for ``rez env bat``"""
+    env.PATH.append('{root}')
